115542340b1648cefa3e5728c2616f428ee7b7e7
여린히읗 <damhwee@icloud.com>
2023-11-10 01:23:35 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+if (window.location.href === "https://webdt.edunet.net/pdf") {
+    console.log("extracting...");
+} else if (window.location.href === "https://webdt.edunet.net/viewer/") {
+    console.log("viewer is not supported");
+} else {
+    console.log("wrong url.");
+}
+
+var scriptText = document.querySelectorAll('body > script:nth-child(35)')[0].textContent;
+
+var match = scriptText.match(/if \("pdf" == "pdf"\) {\s+parent\.contentInformationURL = "(.*?)";/);
+if (match) {
+    var pdfLink = match[1];
+    console.log("extracted pdf link:", pdfLink);
+
+    fetch(pdfLink, { headers: { 'Referer': 'https://webdt.edunet.net/' } })
+        .then(response => response.blob())
+        .then(blob => {
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = document.querySelector('#toc_bookNm').textContent + ".pdf";
+            link.click();
+        })
+        .catch(error => console.error("error:", error));
+} else {
+    console.log("failed to extract pdf url");
+}
